d275d0ae4eb664534d2e8db8443f408eddf2fca7
Steffen Schmitz <steffen@langfuse.com>
2025-03-10 15:57:10 +0100

    feat: add langfuse quarkus example application (#1)

---
+++ applications/quarkus-langchain4j-demo/src/main/java/com/langfuse/quarkus/MyAiService.java
@@ -0,0 +1,24 @@
+package com.langfuse.quarkus;
+
+import dev.langchain4j.service.SystemMessage;
+import dev.langchain4j.service.UserMessage;
+import io.quarkiverse.langchain4j.RegisterAiService;
+
+@RegisterAiService()
+public interface MyAiService {
+
+    /**
+     * Ask the LLM to create a poem about the given topic.
+     *
+     * @param topic the topic of the poem
+     * @param lines the number of line of the poem
+     * @return the poem
+     */
+    @SystemMessage("You are a professional poet")
+    @UserMessage("""
+            Write a single poem about {topic}. The poem should be {lines} lines long and your response should only include the poem itself, nothing else.
+            Then send this poem by email. Your response should include the poem.
+            """)
+    String writeAPoem(String topic, int lines);
+
+}
